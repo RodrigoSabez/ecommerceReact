4ce20f2d45ad1c2f694f2a571feceeaa78854948
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-05-31 22:53:28 -0300

    agregando archivos jsx para entrega 01/06

---
+++ components/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import carrito from '../imagenes/carrito.png'
+
+
+export default function CartWidget() {
+  return (
+   <img className='carrito' src={carrito} alt="" />  
+
+  )
+}
