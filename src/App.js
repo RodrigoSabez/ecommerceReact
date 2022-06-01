cd1c32ac8562702ffd04bcfa75b670754404aa4e
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-05-31 22:57:21 -0300

    modificaciones para entrega 01/06

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import NavBar from './components/NavBar';
 import Saludar from './components/Saludar';
 import "bootstrap/dist/css/bootstrap.min.css";
+import CartWidget from './components/CartWidget';
+import ItemListContainer from './components/ItemListContainer';
  
 
 function App() {
   return <div>
-    <h1>Bienvenidos al ecommerce</h1>
+    <h1>Bienvenidos al ecommerce NF</h1>
     
     <NavBar />
+    <CartWidget />
+    <ItemListContainer greetings={'Saludo para el desafio'}/>
 
   </div>
 
