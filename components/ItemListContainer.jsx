77b36feeec3c4d39956ca636ccc720b9dc11416d
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-06-06 19:49:44 -0300

    Delete components directory

--- components/ItemListContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function ItemListContainer({ greetings }) {
-  return  <h1>{ greetings }</h1>;
-  
-}
-
-export default ItemListContainer
--- components/Saludar.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Saludar({ msg, quien}) {
-    return <div> {msg + " " + quien} </div>;
-}
