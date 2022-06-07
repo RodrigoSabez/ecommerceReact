014b77f1dfe84e556eec782b961b94f4e4de1c32
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-06-07 10:09:30 -0300

    agregando archivos para entrega 06/06

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 
 import NavBar from './components/NavBar';
 import Saludar from './components/Saludar';
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import CartWidget from './components/CartWidget';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount';
  
 
 function App() {
+
+  const onAdd = (count) => {
+    alert(`Sumaste ${count} productos`)
+  }
+
   return <div>
     <h1>Bienvenidos al ecommerce NF</h1>
     
     <NavBar />
     <CartWidget />
     <ItemListContainer greetings={'Saludo para el desafio'}/>
+    <ItemCount  inicial={1} max={10} onAdd={onAdd} />
 
   </div>
 
