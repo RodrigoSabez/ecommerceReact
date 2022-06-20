00836148171293f24e622085c5bcecf632fef036
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-06-20 18:39:22 -0300

    entrega counter

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
+import './App.css';
 import NavBar from './components/NavBar';
-// import "bootstrap/dist/css/bootstrap.min.css";
-import CartWidget from './components/CartWidget';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ItemListContainer from './components/ItemListContainer';
-import {useState} from 'react';
-import ItemDetailContainer from "./components/ItemDetailContainer";
-
-
- 
+import ItemDetailContainer from './components/ItemDetailContainer';
+import Cart from './components/Cart';
 
 function App() {
-
-
-  const [carrito, setCarrito] = useState(0);
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="producto/:id" element={<ItemDetailContainer />} />
+        <Route path="/cart" element={ <Cart/> } />
+      </Routes>
+    </BrowserRouter>
     
-  const sumarCarrito = (cantidad) => {
-    setCarrito(carrito + cantidad)
-  }
-
-  const onAdd = (count) => {
-    alert(`Sumaste ${count} productos`)
-  }
-return (
- <div>
-    <h1>Bienvenidos al ecommerce NF</h1>
-    
-    <NavBar />
-    <CartWidget />
-
-   {/* <ItemListContainer  greetings={'Saludo para el desafio'}  /> */  }
-    <ItemDetailContainer />
-
-  </div>
-)
-  
+  );
 }
 
 export default App;
-
-
