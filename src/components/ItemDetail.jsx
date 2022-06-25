b8ae17b7b378c7ac59d38ca9e8d0786d42cc302a
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-06-25 01:49:24 -0300

    modificaciones re-entrega counter

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from "react-router-dom";
 import ItemCount from './ItemCount'
+import { useState } from 'react';
 
-const ItemDetail = ({ product }) => {
+export default function ItemDetail ( { product } ) {
+    const { title, detail, price, img, id } = product;
+    
+    const {addToCart} = useContext
+
+    const [ setCant] = useState(0);
+  
+  
+    //FUNCIONES DE ITEM COUNT
+    const [count, setCount] = useState(1)
+    const stock = 10;
+  
+    const sumar = () => {
+      count < stock ? setCount(count + 1) : alert('No hay suficiente stock')
+    }
+    const restar = () => {
+      count > 1 ? setCount(count - 1) : alert('La cantidad no puede ser menor que 1')
+    }
+    const reset = () => {
+      setCount(1)
+    }
+  
+    const agregar = (count) => {
+      if (count === 1) {
+        alert(`Se agregó ${title} al carrito`);
+      } else {	
+      alert(`Se agregaron ${count} ${title} al carrito.`);
+      }
+      setCant(count);
+      addToCart(detail, count);
+    }
+  
+
+  //FIN FUNCIONES DE ITEM COUNT
 
     return (
         <div className="card text-center">
@@ -13,11 +47,9 @@ const ItemDetail = ({ product }) => {
                 <h5 className="card-title">{product.title}</h5>
                 <img src={product.img} alt={product.title} />
                 <p className="card-text">{product.detail}</p>
-                <ItemCount />
+                <p className="card-text"><h3>${product.price}</h3></p>
+               <br /> <ItemCount  stock={10} initial={1} onAdd={agregar} sumar={sumar} restar={restar} reset={reset} count={count} />
                 <Link to="/" className="btn btn-dark">Volver</Link>
-            </div>
-            <div className="card-footer text-muted">
-                ${product.price}
             </div>
 
         </div>
@@ -25,5 +57,3 @@ const ItemDetail = ({ product }) => {
 
     )
 }
-
-export default ItemDetail
