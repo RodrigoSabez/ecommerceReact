74fcf93dffd6dadb9b27fda3dcf462b77ca83ce3
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-06-29 23:33:31 -0300

    modificacion entrega 29/06

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import Cart from './components/Cart';
+import MyProvider from './context/CartContext';
 
 function App() {
   return (
     <BrowserRouter>
+     <MyProvider>
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContainer />} />
         <Route path="producto/:id" element={<ItemDetailContainer />} />
         <Route path="/cart" element={ <Cart/> } />
       </Routes>
+     </MyProvider>
     </BrowserRouter>
     
   );
