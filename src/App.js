027251220f1580b4e8a5dec416ee2b559e410d74
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-07-26 21:10:35 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
-import ItemListContainer from './containers/ItemListContainer';
-import ItemDetailContainer from './containers/ItemDetailContainer';
+import ItemListContainer from './components/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import About from './Routes/About';
-import Contact from './Routes/Contact';
+import About from './routes/About';
+import Contact from './routes/Contact';
 import Footer from './components/Footer';
 import MyProvider from './context/CartContext';
 import Cart from './components/Cart';
@@ -13,13 +13,13 @@ import { initializeApp } from "firebase/app";
 import Checkout from './components/Checkout';
 
 initializeApp({
-  apiKey: "AIzaSyARqyjefhTlACUQM2KARBft_goUk0QBs9Q",
-  authDomain: "react-davico.firebaseapp.com",
-  projectId: "react-davico",
-  storageBucket: "react-davico.appspot.com",
-  messagingSenderId: "1069044923785",
-  appId: "1:1069044923785:web:e8e27bb791a7a9cd6c7558",
-  measurementId: "G-1L3MDGK65J"
+  apiKey: "AIzaSyBnQGFdUfPx7TeEAs0ZI5Ekj5-sK1FyVNE",
+  authDomain: "nuestrosfuegos.firebaseapp.com",
+  projectId: "nuestrosfuegos",
+  storageBucket: "nuestrosfuegos.appspot.com",
+  messagingSenderId: "992607824504",
+  appId: "1:992607824504:web:5181d4aec025782335db8d",
+  measurementId: "G-SPZV52S22T"
 });
 
 
@@ -31,10 +31,10 @@ function App() {
         <NavBar />
           <Routes>
             <Route path="/" element={<ItemListContainer/>}/>
-            <Route path="/category/:id" element={<ItemListContainer category_id={"MLA429175"}/>}/>
+            <Route path="/category/:id" element={<ItemListContainer category_id={"items"}/>}/>
             <Route path="/about" element={<About/>} />
             <Route path="/contact" element={<Contact/>} />
-            <Route path="/producto/:id" element={<ItemDetailContainer/>} />
+            <Route path="/productos/:id" element={<ItemDetailContainer/>} />
             <Route path="/cart" element={<Cart/>} />
             <Route path="/checkout" element={<Checkout/>} />
           </Routes>
