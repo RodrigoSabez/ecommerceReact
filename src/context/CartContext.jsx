5fc4a7735f5e1b48e752d51b68afe04094fecea6
RodrigoSabez <98352366+RodrigoSabez@users.noreply.github.com>
2022-07-26 21:16:06 -0300

    Add files via upload

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,54 +1,88 @@
 import { createContext, useState } from "react";
+import swal from "sweetalert";
 
 export const CartContext = createContext()
-
 const {Provider} = CartContext;
-
 const MyProvider = ({children}) => {
+    
+    const [cart, setCart] = useState([]);
 
-    const [cart, setCart] = useState([])
-//
-const isInCart = (id) => {
-    return cart.some(x => x.id === id)
-}
-//
-const addItem = (item, stock) => {
-    const newItem = {
-        ...item,
-        stock
+    //Is in cart determina si el producto a agregar ya está en el carrito o no
+    const isInCart = (id) => {
+        return cart.some((detail) => detail.id === id);
     }
 
-    if(isInCart(newItem.id)){
-        const findProduct = cart.find(x => x.id ===newItem.id)
-        const ProductIndex = cart.indexOf(findProduct)
-        const auxArray = [...cart]
-        auxArray[ProductIndex].stock += stock
-        setCart(auxArray)
-    } else{
-        setCart([...cart], newItem)
+    //AddToCart agrega un producto al carrito, o aumenta la cantidad si ya está en el carrito
+    const addToCart = (detail, count, id) => {
+        const newItem = {
+            ...detail,
+            count,
+            id,
+        };
+        if (isInCart(newItem.id)) {
+            const findProduct = cart.find(item => item.id === newItem.id);            
+            const productIndex = cart.indexOf(findProduct);
+            const auxArray = [...cart];
+            const total = auxArray[productIndex].count += count;
+            if (total > newItem.stock) {
+                swal("Error", `No hay suficientes unidades en stock`, "error");
+            }
+            else {
+                setCart(auxArray);
+                window.localStorage.setItem(id, JSON.stringify(auxArray[productIndex]));
+            }
+        }
+        else {
+            setCart([...cart, newItem]);
+            window.localStorage.setItem(id, JSON.stringify(newItem));
+        }
     }
-}
-//
-const emptyCart = () => {
-    setCart([])
-}
-//
-const deleteItem = (id) => {
-    return cart.filter(x => x.id !== id)
-}
-//
-const getItemQty = () => {
-    return cart.reduce((acc, x) => acc += x.stock, 0)
-}
-//
-const getItemPrice = () => {
-    return cart.reduce((acc, x) => acc += x.stock * x.price)
-}
 
+    //Poblar Checkout con LocalStorage
+    const populateCart = () => {
+        let arrayOfValues = Object.values(localStorage);
+        if (arrayOfValues.length > 0) {
+            let arrayOfProducts = [];
+            for (let i = 0; i < arrayOfValues.length; i++) {
+                arrayOfProducts.push(JSON.parse(arrayOfValues[i]));
+            }
+            setCart(arrayOfProducts);
+        }    
+    }
 
+    //EmptyCart vacia el carrito
+    const emptyCart = () => {
+        setCart([]);
+        localStorage.clear();
+    }
 
+    //RemoveFromCart elimina un producto cuyo ID coincida con el que se le pasa por parámetro.
+    const removeFromCart = (id) => {
+        const newCart = cart.filter(item => item.id !== id);
+        setCart(newCart);
+        window.localStorage.removeItem(id);
+    }
+    
+    //GetItemQuantity retorna la cantidad total de unidades que tiene nuestro state cart
+    const getItemQuantity = () => {
+        return cart.reduce((acc, x) => acc += x.count , 0)
+    }
+    
+    //GetCartTotal retorna el precio total de todos los productos que tiene el carrito
+    const getCartTotal = () => {
+        return cart.reduce((acc, x) => (acc += x.price * x.count) , 0)
+    }
 
-    return <Provider value={{cart, isInCart, addItem, deleteItem, emptyCart, getItemQty, getItemPrice}}>{children}</Provider>
+    
+    return <Provider value={{
+        cart,
+        isInCart,
+        addToCart,
+        emptyCart,
+        removeFromCart,
+        getItemQuantity,
+        getCartTotal,
+        populateCart
+    }}>{children}</Provider>
 }
-
-export default MyProvider
+export default MyProvider;
